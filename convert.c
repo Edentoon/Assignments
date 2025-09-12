89b54566518f07f52e62b552ff1d8cf6c70e8be8
edentoon <edenstesfaye@gmail.com>
2025-09-12 13:17:30 -0400

    more fixes

--- convert.c
+++ convert.c
@@ -2,19 +2,12 @@
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
+// Global variable for passed tests
 int passed = 0;
-void div_convert(uint32_t n, int base, char *out);
-void sub_convert(uint32_t n, int base, char *out);
-void print_tables(uint32_t n);
-/*int main()
-{
-    char bin[33];
-    sub_convert(8,2,bin);
-    printf("%s",bin);
-} */
 
 void div_convert(uint32_t n, int base, char *out) 
 {
+    
     // Accounting for base case
     if (n == 0)
     {
@@ -55,7 +48,7 @@ void div_convert(uint32_t n, int base, char *out)
     {
         final[i] = buffer[pos-(i+1)];
     }
-    final[length+1] = '\0';
+    final[length] = '\0';
 
     // Outputting final result
     strcpy(out,final);
@@ -72,30 +65,25 @@ void sub_convert(uint32_t n, int base, char *out)
     char final[60];
     int pos = 0;
 
-    int exp = 0;
-    int mult = 0;
-    int num;
+    uint64_t exp = 0;
+    uint64_t mult = 0;
+    uint64_t num;
 
     // Subtraction algorithm
     while (pow(base,exp+1)<= n)
     {
         exp++;
     }
+
     do 
     {
         num = pow(base,exp);
-        if (n==0)
-        {
-            mult = 0;
-        }
-        else 
-        {
-            while (num*(mult+1)<=n)
-            {
-                mult++;
-            }
-        }
         
+        while (num*(mult+1)<=n)
+        {
+            mult++;
+        }
+       
         n -= num*mult;
 
         // Building final string
@@ -108,58 +96,26 @@ void sub_convert(uint32_t n, int base, char *out)
             final[pos] = 'A' + (mult-10);
         }
         pos++;
-        exp--;  
+        if (exp == 0)
+        {
+            break;
+        }
+        exp--;
+        mult=0;  
     }
     while (exp>= 0);
+
+    // Finishing string
     final[pos] = '\0';
     strcpy(out,final);
-    
-    /*while (n>base)
-    {
-        int
-        
-
-        // Extracting each digit by subtracting multiples of powers
-        while (pow(base,exp+1)<= n)
-        {
-            exp++;
-        }
-        printf("%d\n",exp);
-        int num = pow(base,exp);
-        while (num*(mult+1)<n)
-        {
-            mult++;
-        }
-        printf("%d\n",mult);
-        n -= num*mult;
-        
-        
-        if (mult <10)        
-        {
-            final[pos] = '0' + mult;
-        }
-        else
-        {
-            final[pos] = 'A' + (mult-10);
-        }
-        pos++;  
-    }
-
-    if (n<10)
-    {
-        final[pos] = '0' + n;
-    }
-    else
-    {
-        final[pos] = 'A' + (n-10);
-    }
-    final[pos+1] = '\0';
-    strcpy(out,final); */
 }
+
 void print_tables(uint32_t n)
 {
-    int shift_check = n*8;
-    int mask_check;
+    // Mathematical tests for bit operations
+    uint32_t shift_check = n*8;
+    uint32_t mask_check;
+
     if (n>255)
     {
         mask_check = 255;
@@ -169,28 +125,27 @@ void print_tables(uint32_t n)
         mask_check = n;
     }
     
-
-    char bin[33], oct[12], hex[9];
+    char bin[50], oct[12], hex[9];
 
     div_convert(n,2,bin);
     div_convert(n,8,oct);
     div_convert(n,16,hex);
 
-    printf("Original: Binary=%s Octal=%s Decimal=%d Hex=%s\n", bin, oct, n, hex);
+    printf("Original: Binary=%s Octal=%s Decimal=%u Hex=%s\n", bin, oct, n, hex);
 
     uint32_t shift = n << 3;
     div_convert(shift,2,bin);
     div_convert(shift,8,oct);
     div_convert(shift,16,hex);
     
-    printf("Left shift by 3: Binary=%s Octal=%s Decimal=%d Hex=%s\n", bin, oct, shift, hex);
+    printf("Left shift by 3: Binary=%s Octal=%s Decimal=%u Hex=%s\n", bin, oct, shift, hex);
 
     uint32_t mask = n & 0xFF;
     div_convert(mask,2,bin);
     div_convert(mask,8,oct);
     div_convert(mask,16,hex);
 
-    printf("AND with 0xFF: Binary=%s Octal=%s Decimal=%d Hex=%s ", bin, oct, mask, hex);
+    printf("AND with 0xFF: Binary=%s Octal=%s Decimal=%u Hex=%s\n", bin, oct, mask, hex);
     
     char result[5];
     if (mask_check == mask && shift_check == shift)
--- main.c
+++ main.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Global variable for tracking test number
 extern int passed;
 
 int main()
 {
+
     FILE *testfile = fopen("a1_test_file.txt","r");
     if ( testfile == NULL ) 
     {
@@ -14,23 +16,27 @@ int main()
         return 1;
     }
     
+    // Allocating buffer space
     char buff[100];
-    int test = 1;
+    int test = 0;
     
-
+    // Reading testfile line by line
     while (fgets(buff,100,testfile)!= NULL)
     {
         // Checking for which function to test
         if (buff[0] == 'd' || buff[0] == 's' || buff[0] == 'p')
         {
+            // Tracking test number
+            test++;
+
             uint32_t a,b;
             char func[12];
             char compare[33];
             char output[33];
             char result[5];
-
+            
             // Separate instructions for div and sub convert
-            if (buff[0] == 'd' | 's')
+            if (buff[0] == 'd' || buff[0] == 's')
             {
                 // Conversion reading format
                 sscanf(buff, "%s %d %d %s", func,&a,&b,compare);
@@ -55,21 +61,20 @@ int main()
                     strcpy(result,"FAIL");
                 }
 
-                // Printing final results to file
-                printf("Test %d: %s(%d,%d) -> Expected: \"%s\", Got: \"%s\" [%s]\n", test,func,a,b,compare,output,result);
+                // Printing final results to screen
+                printf("Test %d: %s(%u,%d) -> Expected: \"%s\", Got: \"%s\" [%s]\n", test,func,a,b,compare,output,result);
                 
             }
             else 
             {
                 // Print tables reading format
                 sscanf(buff,"%s %d %s", func, &a, compare);
-                printf("Test %d: %s(%d) -> ");
+                
+                printf("Test %d: %s(%d) -> \n",test,func,a);
                 print_tables(a); 
             }
-            test++;
         }
-        
     }
-    //fprintf(outputfile,"Summary: %d/%d tests passed\n",passed,test);
-    //fclose(outputfile);
+    printf("Summary: %d/%d tests passed\n",passed,test);
+    fclose(testfile);
 }
